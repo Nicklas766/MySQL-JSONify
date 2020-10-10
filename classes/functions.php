56cf54d2c92b419dcec861feb4ee8993c057d347
Reqrefusion <39885728+Reqrefusion@users.noreply.github.com>
2020-10-11 00:37:37 +0300

    MySql-JSONify 3.1
    +A more advanced and easy filter system.

--- classes/functions.php
+++ classes/functions.php
@@ -15,7 +15,11 @@ function arrayKeyRemove($arr, $key) {
 
 // The function returns the GET based on key.
 function getGet($key, $default = null) {
-    return isset($_GET[$key]) ? $_GET[$key] : $default;
+    if (isset($_GET[$key])) {
+        return $_GET[$key];
+    } else {
+        return $default;
+    }
 }
 
 // The function returns the POST based on key.
@@ -88,62 +92,96 @@ function orderOrganizer($order, $tableRows, $idCol) {
     return $organizeOrder;
 }
 
+
+function btComOperatorOrganizer($col, $searchWords) {
+	asort($searchWords);
+    $searchFirst = current($searchWords);
+    $searchEnd = end($searchWords);
+            return "`".$col."` BETWEEN '".$searchFirst."' AND '".$searchEnd."' ";
+	
+}
+function filterClauseOrganizer($col, $comOperator, $searchWords) {
+    $search = current($searchWords);
+    switch ($comOperator) {
+        case "lk"://LIKE
+            return "`".$col."` LIKE '".$search."' ";
+        case "nlk"://NOT LIKE
+            return "`".$col."` NOT LIKE '".$search."' ";
+        case "cs"://contain string (string contains value)
+            return "`".$col."` LIKE '%".$search."%' ";
+        case "sw"://start with (string starts with value)
+            return "`".$col."` LIKE '".$search."%' ";
+        case "ew"://end with (string end with value)
+            return "`".$col."` LIKE '%".$search."' ";
+        case "eq"://equal (string or number matches exactly)
+            return "`".$col."` = '".$search."' ";
+        case "lt"://lower than (number is lower than value)
+            return "`".$col."` > '".$search."' ";
+        case "le"://lower or equal (number is lower than or equal to value)
+            return "`".$col."` => '".$search."' ";
+        case "ge"://greater or equal (number is higher than or equal to value)
+            return "`".$col."` =< '".$search."' ";
+        case "gt"://greater than (number is higher than value)
+            return "`".$col."` < '".$search."' ";
+        case "bt"://between (number is between two comma separated values)
+            return btComOperatorOrganizer($col, $searchWords);
+        case "in": //in (number or string is in comma separated list of values)
+            return "`".$col."` IN (".implode(",", array_map("addStartEndSingleQuote",$searchWords)).") ";
+        case "is"://is null (field contains "NULL" value)
+            return "`".$col."` IS NULL ";
+        default: //cace cs(?):
+            return "`".$col."` LIKE '%".$search."%' ";
+    }
+}
+
 function filterOrganizer($filter, $tableRows) {
-        $comOperatorsArray = array("LIKE", "NOT LIKE", );
-        $logOperatorsArray = array("AND", "OR", "||", "&&", "XOR");
-        $slices = explode(";", $filter);
-        $organizeFilter = "";
-        foreach($slices as $slice) {
-            $comOperator = "LIKE";
-            $logOperator = "OR";
-            $search = "";
-            $parts = explode(",", $slice);
-            foreach($parts as $part) {
-                if (arrayCheckIn($part, $comOperatorsArray)) {
-                    $comOperator = arrayCheck($part, $comOperatorsArray);
-                } elseif (arrayCheckIn($part, $tableRows)) {
-                    $col = arrayCheck($part, $tableRows);
-                } elseif (arrayCheckIn($part, $logOperatorsArray)) {
-                    $logOperator = arrayCheck($part, $logOperatorsArray);
-                } else {
-                    $search = sqlStringEscaper($part);
-                }
-            }
-            if (empty($col)) {
-                $organizeFilter .= "(";
-                foreach($tableRows as $tableRow) {
-                    $organizeFilter .= "`".$tableRow.
-                    "` ".$comOperator.
-                    " '".$search.
-                    "' ";
-                    if ($tableRow !== end($tableRows)) {
-                        $organizeFilter .= "OR".
-                        " ";
-                    }
-                    elseif($slice !== end($slices)) {
-                        $organizeFilter .= ") ".$logOperator.
-                        " ";
-                    } else {
-                        $organizeFilter .= ") ";
-                    }
-                }
-            } else {
-                $organizeFilter .= "`".$col.
-                "` ".$comOperator.
-                " '".$search.
-                "' ";
-                if ($slice !== end($slices)) {
-                    $organizeFilter .= $logOperator.
-                    " ";
-                }
+    $comOperatorsArray = array("lk", "nlk", "cs", "sw", "ew", "eq", "lt", "le", "ge", "gt", "bt", "in", "is");
+    $logOperatorsArray = array("AND", "OR", "||", "&&", "XOR");
+    $slices = explode(";", $filter);
+    $organizeFilter = "";
+
+    foreach($slices as $slice) {
+        $comOperator = "LIKE";
+        $logOperator = "OR";
+        $search = "";
+        $parts = explode(",", $slice);
+        $comOperator = current(array_intersect($parts, $comOperatorsArray));
+        $logOperator = current(array_intersect($parts, $logOperatorsArray));
+        $colIntersect = array_intersect($parts, $tableRows);
+        $comLogOperatorsColDiff = array_diff($parts, array_merge($comOperatorsArray, $logOperatorsArray, $tableRows));
+        $searchWords = array_map("sqlStringEscaper", $comLogOperatorsColDiff);
+
+        if (empty($colIntersect)) {
+            $cols = $tableRows;
+        } else {
+            $cols = $colIntersect;
+        }
+        $organizeFilter .= "(";
+        foreach($cols as $col) {
+            $organizeFilter .= filterClauseOrganizer($col, $comOperator, $searchWords);
+            if ($col !== end($cols)) {
+                $organizeFilter .= "OR".
+                " ";
             }
         }
-        return $organizeFilter;
+        $organizeFilter .= ") ";
+
+
+
+        if ($slice !== end($slices)) {
+            if (empty($logOperator)) {
+                $logOperator = "OR";
+            }
+            $organizeFilter .= $logOperator.
+            " ";
+        }
     }
+    return $organizeFilter;
+}
+	
 // Update statement 
 function updateOrganizer($table, $posts, $idCol) {
-    $sql = "UPDATE ".$table.
-    " SET ";
+    $sql = "UPDATE `$table` SET ";
     foreach($posts as $key => $value) {
         if ($key !== $idCol) {
             $sql = $sql.$key.
@@ -178,14 +216,15 @@ function returnInfo($data, $sql, $connect) {
         $info["login"] = $data -> loginJwt($connect);
     }
     elseif($params["token"]) {
-        $info["login"]['token'] = $params["token"];
         $info["posts"] = $data -> posts;
         try {
             $payload = JWT::decode($params["token"], $data -> serverKey, array('HS256'));
             $info["login"] = $payload;
+
         } catch (Exception $e) {
-            $info["login"] = $e -> getMessage();
+            $info["login"]['error'] = $e -> getMessage();
         }
+        $info["login"]['token'] = $params["token"];
     }
     $info["id"] = $data -> idCol;
     return $info;
